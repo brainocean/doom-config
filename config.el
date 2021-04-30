a259782b549aec3911ab2a68796c841eaef53b38
brainocean <brainoean@qq.com>
2021-04-30 17:59:59 +0800

    Seems we don't need to set org table fonts.

--- config.el
+++ config.el
@@ -31,9 +31,9 @@
 
 (defun set-chinese-font (size)
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-                    charset
-                    (font-spec :family "PingFang SC" :size size))))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "PingFang SC" :size size))))
 
 
 (defun set-org-table-font (size)
@@ -41,7 +41,7 @@
   (set-face-attribute 'org-table nil :font (font-spec :family "Sarasa Mono SC" :size size)))
 
 (set-chinese-font normal-font-size)
-(after! org (set-org-table-font normal-font-size))
+;; (after! org (set-org-table-font normal-font-size))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -66,7 +66,7 @@
 (defun on-writeroom-mode ()
   (set-special-fonts writeroom-mode))
 
-(add-hook 'writeroom-mode-hook #'on-writeroom-mode)
+;; (add-hook 'writeroom-mode-hook #'on-writeroom-mode)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
